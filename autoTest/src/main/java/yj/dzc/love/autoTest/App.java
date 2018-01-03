87d67386a035bd863c85f106ce57c685b7482687
yj6326076 <yj6326076@hotmail.com>
2018-01-03 10:34:43 +0800

    new a project

---
+++ autoTest/src/main/java/yj/dzc/love/autoTest/App.java
@@ -0,0 +1,13 @@
+package yj.dzc.love.autoTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
